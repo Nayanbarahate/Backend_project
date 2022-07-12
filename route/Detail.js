f6b6b21a78c201e7cc654f609c748813ac2ec449
Nayanbarahate <nayanbarahate11@gmail.com>
2022-07-12 14:35:39 +0530

    add files

---
+++ route/Detail.js
@@ -0,0 +1,10 @@
+// const express=require("express");
+
+// const detailControler = require("../component/Hollywood")
+
+// const detailRouter=express.Router()
+
+// detailRouter.route("/detail")
+// .get(detailControler.apiController)
+
+// module.exports=detailRouter
